d126cd425536d0a5c2142b646e798f9ef7ac1b24
Aishwarya <riya129ash@gmail.com>
2024-01-31 19:33:13 +0530

    route

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 const express = require("express");
 const cors = require("cors");
+const userRoutes = require("./user/user");
+const productRoutes = require("./product/product");
+
 const app = express();
+const router = express.Router();
 
 app.use(
   cors({
     credentials: true,
   })
 );
+app.use(express.json());
 
-app.get("/", function (req, res) {
-  res.send("Hello World!");
-});
+// app.use("/user", userRoutes);
+// app.use("/product", productRoutes);
 
 app.listen(3000, function () {
-  console.log("Example app listening on port 3000!");
+  console.log(`Example app listening on port 3000!`);
 });
 
-const server = http.createServer(app);
+app.get("/", function (req, res) {
+  res.send("Hello there!");
+});
+
+app.get("/user", function (req, res) {
+  res.send("Hello User!");
+});
+
+app.get("/product", function (req, res) {
+  res.send("Hello product!");
+});
---
+++ product/product.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+// const productRouter = express.Router();
+
+// productRouter.get("/", async (req, res) => {
+//   try {
+//     const products = await ProductService.getAllProducts();
+//     return res.status(200).json(products);
+//   } catch (e) {
+//     console.log(e);
+//   }
+// });
